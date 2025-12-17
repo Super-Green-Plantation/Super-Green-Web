f3bb53dd482252770bfa51750d81364d1d68bcec
Super-green <supergreenitdep@gmail.com>
2025-12-17 09:12:21 +0530

    fix dynamic estate issue

--- my-app/app/estates/[id]/page.tsx
+++ my-app/app/estates/[id]/page.tsx
@@ -1,10 +1,51 @@
+export default function page({ params}: {params: { id: string };}){
 
 
-const page = ({params}:any) => {
-    const id = params.id
+  console.log("ID →", params.id);
+
+  const id = parseInt(params.id)
+   
+
+   const estate = [
+    {
+      id: 1,
+      img: "/sample-1.jpg",
+      location: "Galle",
+    },
+    {
+      id: 2,
+      img: "/sample-2.jfif",
+      location: "Matara",
+    },
+    {
+      id: 3,
+      img: "/sample-3.jfif",
+      location: "Hambantota",
+    },
+    {
+      id: 4,
+      img: "/sample-4.jfif",
+      location: "Rathnapura",
+    },
+    {
+      id: 5,
+      img: "/sample-5.jpg",
+      location: "Thanamalwila",
+    },
+  ];
+
   return (
-    <div className='mt-10'>dynamic estates {id}</div>
-  )
-}
+    <>
+    <div className="mt-5">
 
-export default page
+      {estate.map((es, index)=>(
+        <div key={index}>
+          <img src={es.id == id ? es.img : ""} alt="" />
+          <h2>{es.id == id ? es.location : ""}</h2>
+        </div>
+      ))}
+
+    </div>
+    </>
+  );
+}
