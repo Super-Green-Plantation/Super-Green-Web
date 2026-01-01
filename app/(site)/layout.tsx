8088319f13ede69508f148a02beb2418868a501f
Supun <infor.ssupun@gmail.com>
2026-01-01 15:05:50 +0530

    --metadata

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from "next";
 import "../globals.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/Navbar";
 
+export const metadata: Metadata = {
+  title: "Super Green Plantation",
+  description: "Super Green Plantation - Your Trusted Partner in Sustainable Agriculture",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 
 export default function RootLayout({
